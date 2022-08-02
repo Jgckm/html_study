c61d4bf5c33456114eb41fac834fbb481b19dd7b
jgckM <z3515235474@163.com>
2022-08-02 15:31:57 +0800

    vue笔记

--- vue_base/07-单文件组件/test2008/src/views/Detail.vue
+++ vue_base/07-单文件组件/test2008/src/views/Detail.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>detail</div>
+  <div v-if="filmInfo">
+    <!-- <img :src="filmInfo.poster" alt=""> -->
+    <div class="poster" :style="{backgroundImage:`url(${filmInfo.poster})`}"></div>
+    <div class="content">
+      <div class="name">{{filmInfo.name}}</div>
+      <div>
+        <div class="detail-text">{{filmInfo.category}}</div>
+        <div class="detail-text">{{filmInfo.premiereAt | dataFilter}}上映</div>
+        <div class="detail-text">{{filmInfo.nation}} | {{filmInfo.runtime===0 ? '未知':filmInfo.runtime}}</div>
+        <div class="des" :class="hidden ?'hidden':''">{{filmInfo.synopsis}}</div>
+        <div @click="hidden = !hidden" style="text-align:center"><i class="fa" aria-hidden="true" :class="hidden ? 'fa-angle-down':'fa-angle-up'"></i></div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import http from '@/util/http'
+import momnet from 'moment' // 设置中文
+import Vue from 'vue'
 
+momnet.locale('zh-cn')
+Vue.filter('dataFilter', (data) => {
+  return momnet(data * 1000).format('YYYY-MM-DD')
+})
 export default {
   data () {
     return {
-      moiveId: null
+      filmInfo: null,
+      hidden: true
     }
   },
   created () {
@@ -23,9 +43,39 @@ export default {
       }
     }).then(
       (res) => {
-        console.log(res.data.film)
+        console.log(res.data)
+        this.filmInfo = res.data.film
       }
     )
   }
 }
 </script>
+<style lang="scss" scoped>
+.poster{
+  width:100%;
+  height: 10.6667rem;
+  background-position: center;
+  background-size: cover;
+}
+.content{
+  padding: .8333rem;
+  .detail-text{
+    font-size: 13px;
+    color: #797d82;
+    margin-top: .2222rem;
+  }
+}
+.name{
+  font-size: 18px;
+}
+.des{
+  margin-top: .2778rem;
+  font-size: 12px;
+  color:#797d82;
+}
+.hidden{
+   height:1.6667rem;
+   overflow: hidden;
+}
+
+</style>
