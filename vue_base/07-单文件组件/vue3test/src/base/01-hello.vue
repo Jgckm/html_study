e2321e4bf24fcea59ee23c711551064cece129ea
jgckM <z3515235474@163.com>
2022-08-05 18:18:17 +0800

    vue笔记

---
+++ vue_base/07-单文件组件/vue3test/src/base/01-hello.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        hello--- 新写法
+        {{myname}}
+        <button @click="handleClick">change</button>
+        <input type="text" v-model="obj.mytext">
+        {{obj.mytext}}
+    </div>
+</template>
+<script>
+import { reactive } from '@vue/reactivity';
+export default {
+    // vue3老写法或者是vue 写法中 的 beforeCreate ，created 生命周期==== setup
+    data(){
+        return {
+            myname:'kerwin',
+        }
+    },
+    beforeCreate(){
+        console.log('beforeCreate');
+    },
+    setup(){
+        console.log('setup');
+        const handleClick = ()=>{
+            console.log(111);
+        }
+        
+        // 定义一个状态
+       const obj = reactive({
+        mytext:''
+       })
+       return{
+        obj,
+        handleClick
+       }
+    }
+}
+</script>
